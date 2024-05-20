71a65364ad977cc014f2038a0280d5b808cd9cd6
stringpappy <jejeodunayo70@gmail.com>
2024-05-20 04:34:27 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
-"""implementation of the class base model"""
+"""class BaseModel that defines all common attributes/methods for other classes
+"""
 import uuid
-from datetime import datetime
-
+import datetime
 
 
 class BaseModel:
-    """super class"""
-
-    def __init__(self):
-        """initialising"""
+    """superclass"""
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+        vi ba
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        p = datetime.datetime.strptime + \
+                                (value, "+%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, p)
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
 
     def __str__(self):
-<<<<<<< HEAD
-        return "[{}] ({}) {}\
-                ".format(self.__class__.__name__,  self.id, self.__dict__)
-=======
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
->>>>>>> c50db0987b293791bd6180b4b52be8332e04d625
+        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def to_dict(self):
-        stuff_in_dict = self.__dict__.copy()
-        stuff_in_dict['__class__'] = self.__class__.__name__
-        stuff_in_dict['created_at'] = self.created_at.isoformat()
-        stuff_in_dict['updated_at'] = self.updated_at.isoformat()
-        return stuff_in_dict
+        dict_obj = self.__dict__.copy()
+        dict_obj['__class__'] = type(self).__name__
+        dict_obj['created_at'] = self.created_at.isoformat()
+        dict_obj['updated_at'] = self.updated_at.isoformat()
+        return dict_obj
