5a78966d6cef005a0bb1d17942db77ba074ab847
stringpappy <jejeodunayo70@gmail.com>
2024-05-20 04:45:24 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
-        vi ba
+
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
