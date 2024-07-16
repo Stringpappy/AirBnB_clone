c6fd56e0c96a171073e0a464f5fee4dad3a8afe4
stringpappy <jejeodunayo70@gmail.com>
2024-07-15 23:47:41 -0700

    airbnb

--- console.py
+++ console.py
@@ -44,6 +44,19 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
+    def do_create(self, arg):
+        """
+        func that create a new class instance and print its id.
+        """
+        arg_n = parse(arg)
+        if len(arg_n) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg_n[0])().id)
+            storage.save()
+
     def do_show(self, line):
         """
         The func that Prints the string representation of an instance
