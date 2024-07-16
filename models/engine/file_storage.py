e73fe8e2b0e8c5dd885359f0770c5f2da5db77da
stringpappy <jejeodunayo70@gmail.com>
2024-07-16 03:06:14 -0700

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,39 +1,46 @@
+#!/usr/bin/python3
+""" 
+module that serializes instances to a JSON file
+    and deserializes JSON file to instances """
 import json
-"""recreate a BaseModel from another one by using a dictionary rep"""
+import uuid
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
+    """ construct """
     __file_path = "file.json"
-    __object = {}
+    __objects = {}
 
     def all(self):
-        """  returns the dictionary """
-        return self.__object
+        """ return dictionary objects """
+        x = FileStorage.__objects
+        return x
 
     def new(self, obj):
-        """set in key"""
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__object[key] = obj
+        """ func sets in dictionary the obj with key <obj class name>.id """
+        FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
 
     def save(self):
-        """Serializaation of the JSON file"""
-        serialized_objs = {}
-        for key, obj in self.__objects.items():
-            serialized_objs[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(serialized_objs, f)
+        """ func that serializes objectss to the JSON file """
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
+            nw_dict = {key: obj.to_dict() for key, obj in
+                        FileStorage.__objects.items()}
+            json.dump(nw_dict, fname)
 
     def reload(self):
-        """Deserialization"""
-        try:
-            with open(self.__file_path, 'r') as f:
-                obj_to_serialized = json.load(f)
-                for key, obj_dict in obj_to_serialized.items():
-                    cls_name, obj_id = key.split('.')
-                    m = __import__('models.' + cls_name, fromlist=[cls_name])
-                    cls = getattr(m, cls_name)
-                    obj = cls(**obj_dict)
-                    self.__objects[key] = obj
-        except FileNotFoundError:
-            pass
+        """ func that Reload the file """
+        if (os.path.isfile(FileStorage.__file_path)):
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as name_of_file:
+                jsonf = json.load(name_of_file)
+                for key, val in jsonf.items():
+                    FileStorage.__objects[key] = eval(
+                        val['__class__'])(**val)
